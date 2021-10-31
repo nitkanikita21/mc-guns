bed282a12010a54d565029bfc7031621833198c2
nitkanikita21 <waleryvvs1@gmail.com>
2021-10-31 19:13:36 +0200

    change README.md

--- src/main/java/com/nitka/mcguns/proxy/ClientProxy.java
+++ src/main/java/com/nitka/mcguns/proxy/ClientProxy.java
@@ -5,9 +5,10 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
+@Mod.EventBusSubscriber(value = Dist.CLIENT)
 public class ClientProxy {
     @SubscribeEvent
     public static void init(final FMLClientSetupEvent e){
-
+        System.out.println("Привет из клиента");
     }
 }
--- src/main/java/com/nitka/mcguns/proxy/ServerProxy.java
+++ src/main/java/com/nitka/mcguns/proxy/ServerProxy.java
@@ -5,9 +5,10 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.server.FMLServerAboutToStartEvent;
 
+@Mod.EventBusSubscriber(value = Dist.DEDICATED_SERVER)
 public class ServerProxy {
     @SubscribeEvent
     public static void init(final FMLServerAboutToStartEvent e){
-
+        System.out.println("Привет из сервера");
     }
 }
